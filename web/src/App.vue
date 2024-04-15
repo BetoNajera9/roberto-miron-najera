a073aea16ff759d26208222036121a09cb57db74
BetoNajera <ing.betonajera@gmail.com>
2024-04-15 14:33:05 -0600

    :tada: Begin: Start front end project
    Start front end project

---
+++ web/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<Header />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Header from './components/header.vue'
+
+export default defineComponent({
+	components: { Header },
+
+	setup() {},
+})
+</script>
